040572be878cd9bff12b7e8bfd8e1ac321633c35
Hiago Coelho <b8coelho@gmail.com>
2021-09-18 04:18:31 -0300

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import status from 'http-status';
 import sequelize from './database/database';
 import loanRouter from './routers/loanRouter';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
